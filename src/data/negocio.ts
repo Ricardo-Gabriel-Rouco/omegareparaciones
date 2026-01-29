d6916bbd79f3780737d1b43bc8802a62523fa3a0
Ricardo-Gabriel-Rouco <r.g.rouco1@gmail.com>
2026-01-29 17:35:51 -0300

    added consistency for styling and added data.ts to upload dinamic data for contact and name page

---
+++ src/data/negocio.ts
@@ -0,0 +1,7 @@
+export const dataNegocio = {
+  name: "Omega Reparaciones",
+  phone: "+5493484380831",
+  mail: "omega.reparaciones2026@gmail.com",
+  hours: "Lunes a Viernes de 9:00 a 18:00",
+  instagram: "@omega.reparaciones2026",
+};
