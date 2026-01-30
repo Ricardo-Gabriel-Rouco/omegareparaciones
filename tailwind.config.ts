ad76c806a1d784e48a140d768ca462f64f07130a
Ricardo-Gabriel-Rouco <r.g.rouco1@gmail.com>
2026-01-30 01:26:51 -0300

    new componentes and new styles to be more concise

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        'xs': '475px',
+      },
       colors: {
         primary: {
           DEFAULT: '#CBE03F',
@@ -31,6 +34,18 @@ export default {
           DEFAULT: '#D4E453',
           foreground: '#000000'
         }
+      },
+      fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1rem' }],
+        'sm': ['0.875rem', { lineHeight: '1.25rem' }],
+        'base': ['1rem', { lineHeight: '1.5rem' }],
+        'lg': ['1.125rem', { lineHeight: '1.75rem' }],
+        'xl': ['1.25rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.5rem', { lineHeight: '2rem' }],
+        '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+        '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
+        '5xl': ['3rem', { lineHeight: '1' }],
+        '6xl': ['3.75rem', { lineHeight: '1' }],
       }
     }
   },
