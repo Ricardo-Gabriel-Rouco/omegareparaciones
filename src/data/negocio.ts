892180bd9cc0c570217097ef5ae2ed8d7bb251e9
Ricardo-Gabriel-Rouco <r.g.rouco1@gmail.com>
2026-01-29 21:43:38 -0300

    styling almost done, left to test responsive

--- src/data/negocio.ts
+++ src/data/negocio.ts
@@ -1,6 +1,7 @@
 export const dataNegocio = {
   name: "Omega Reparaciones",
-  phone: "+5493484380831",
+  phone: "+54 9 348 4380831",
+  whatsapp: "+5493484380831",
   mail: "omega.reparaciones2026@gmail.com",
   hours: "Lunes a Viernes de 9:00 a 18:00",
   instagram: "@omega.reparaciones2026",
